47a7e803940e4724dabfb398a78bfcebb1ec1885
Antonina <144945887+Chernookaya@users.noreply.github.com>
2023-11-20 00:38:46 +0300

    Update AddRemoveElementsTest.java

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -16,11 +16,10 @@ public class AddRemoveElementsTest {
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup(); //Сравнивает текущую версия браузера с драйвером
-        //и в случае чего выкачивает нужную
-        ChromeOptions options = new ChromeOptions(); //задает хромОпции
+        WebDriverManager.chromedriver().setup(); 
+        ChromeOptions options = new ChromeOptions(); 
         options.addArguments("start-maximized");
-        driver = new ChromeDriver(options); //создаю объект драйвера и передаю хромопции
+        driver = new ChromeDriver(options); 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
